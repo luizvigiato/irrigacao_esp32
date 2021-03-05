eb47e386e3bb3430f4702608e3d76cef0a7e51b4
Luiz H. Vigiato <lh.vigiato@gmail.com>
2021-03-04 21:33:17 -0300

    Modificações texto html

--- code/code.ino
+++ code/code.ino
@@ -10,8 +10,8 @@ WiFiClient client;
 
 String header;
 
-#define temp 19
-#define humi 5
+#define LM35 19
+#define sensor 5
 #define led_modulo 2
 
 
@@ -20,16 +20,15 @@ int humidade = 600;
 int contagem = 0;
 int media = 0;
 int registro;
+bool info_status = false;
 
 void setup() {
   wifiManager.autoConnect("Irrigacao LH v.2");
   Serial.begin(115200);
   EEPROM.begin(256);
-  pinMode(temp, INPUT);
-  pinMode(humi, INPUT);
-  pinMode(led_modulo, OUTPUT);
+  pinMode(LM35, INPUT);
+  pinMode(sensor, INPUT);
   
-
   delay(5000);
   // Boot service irrigation
   Serial.println("Iniciando");
@@ -39,6 +38,8 @@ void setup() {
 //  Serial.println(wifiManager.getWiFiSSID());
 
   server.begin();
+  digitalWrite(led_modulo,LOW);
+
   
 
 }
@@ -46,13 +47,15 @@ void setup() {
 void loop() {
 //  identify connection status
 //  Serial.println(wifiManager.autoConnect());
+//  digitalWrite(led_modulo,HIGH);
 
   client = server.available();
   if(contagem > 60){
     humidade = media / 60;
+    media = 0;
     contagem = 0;
   } else {
-    media += analogRead(humi);
+    media += analogRead(sensor);
 
   }
     
@@ -73,6 +76,7 @@ void loop() {
     client.println(textoHtmlPagina());
   }
   
+  
   contagem++;
   delay(1000);
 
@@ -88,14 +92,47 @@ String textoHtmlPagina(){
                "\r\n");            
   htmlPage += F("<!DOCTYPE HTML>"
                 "<html lang=\"pt-BR\"><head>"
-                "<title>Sistema de Irrigacao</title><meta charset=\"utf-8\"></head><body>"
-                "<center><h1>Sistema de Irrigacao Vigiato v.2</h1>"
-                "<h2>Temperatura atual: ");
-  String color = "red";
+                "<title>Sistema de Irrigacao</title><meta charset=\"utf-8\"></head><body style=\"background-color:#F0FFFF\">"
+                "<center><h1>Sistema de Irrigacao Vigiato v.2.2</h1>"
+                "</br></br>"
 
-  htmlPage += (String) contagem;
-  htmlPage +=   " c </h2><h3>Status de Irrigação: <em style=\"color:";
-  htmlPage += color;
-  htmlPage +=   "\">OFFLINE</em></h3>";
+                "<div>"
+                "<table style=\"font-size: 1.5em;\">"
+                "<tr >"
+                "<td>Temperatura atual:</td>"
+                "<td> ");
+//  temperatura = analogRead(LM35)/1024.0;
+  temperatura = (float(analogRead(LM35))*5/(1023))/0.01;
+
+  htmlPage += (String) temperatura;
+
+  htmlPage += F(
+    " c</td></tr><tr><td>Status de Irrigação: </td><td><em style=\"color:"
+    );
+
+  if(info_status){
+    htmlPage += (String)"green";
+  } else {
+    htmlPage += (String)"red";
+  }
+  htmlPage += F(";\"><b>");
+
+  if(info_status){
+    htmlPage += "ATIVO";
+  } else {
+    htmlPage += "DESLIGADO";
+  }
+    
+  htmlPage += F("</b></em></td>");
+  if(info_status){
+    htmlPage += F("<tr><td>Tempo de Irrigação: </td><td>");
+    htmlPage += contagem;
+    htmlPage += F("</td></tr></table>");
+    
+  } else {
+    htmlPage += F("</tr></table>");
+  }
+
+  
   return htmlPage;  
 }
